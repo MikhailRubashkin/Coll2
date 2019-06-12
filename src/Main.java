dd2855e118a93800bf74d46fd5ab591172d851a9
MikhailRubashkin <pristijpribor@mail.ru>
2019-06-12 15:41:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+//Написать метод countUnique, который принимает целочисленный список в качестве параметра и возвращает количество уникальных целых чисел в этом списке.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] array = {11, 12, 10, 5, 4, 44, 100, 44, 11, 10, 11, 3};
+        System.out.println(array.length);
+
+        Set<Integer> mySet = new HashSet<>();
+        for (int x : array) {
+            mySet.add(x);
+        }
+        System.out.println(mySet.size());
+    }
+}
